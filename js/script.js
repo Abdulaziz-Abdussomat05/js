8497c0532f7abefc458bfabb26f0f63b22dd8d3b
Abdulaziz-Abdussomat05 <abdussomatabdulaziz7@gmail.com>
2022-11-17 19:31:34 -1200

    Creating new js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+"use strict";
+let numberCard;
+alert("Assalamu alaykum, nima gap oka, kiberhavfsizlik markaziga xush kelibsiz! Iltimos so`rovni to`ldirishdan avval karta raqamingizni kiriting")
+prompt("Karta raqamni kiriting, farqi yo`q uzcard,xumo.")
+let answers = {
+  age: prompt("Yoshingiz nechida?"),
+  firstName: prompt("ismingizni kiriting"),
+  isLong: prompt("Qayerliksiz"),
+  email: prompt("Emailingizni kiriting"),
+}
+
+console.log(answers );
